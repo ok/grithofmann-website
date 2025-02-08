32c47b06a661ca0ecf8efb88e0879cd4517b95f1
oliver <o.kohl@sap.com>
2025-02-08 13:39:07 +0100

    post/purge css integrated

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
 export default function(eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
-
-  eleventyConfig.addPassthroughCopy("src/css");
-  // eleventyConfig.addPassthroughCopy("src/img"); check scripts/image-transform.js
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy({ "src/public": "." });
