59ebe89bab913d8873fbbcc5936181658ace4ff3
oliver <o.kohl@sap.com>
2025-02-09 14:43:23 +0100

    fix for build setup

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,12 @@
+import "dotenv/config"; // Loads .env file
+ 
+const isDevelopment = process.env.NODE_ENV === "development";
+console.log(`landscape env: ${process.env.NODE_ENV}`);
+
 export default {
   title: "Grit Hofmann",
   description: "Grit Hofmann | Journalist & Marathon Runner",
   keywords: "travel, hiking, photography, portfolio",
-  domain: "https://grithofmann.netlify.app",
+  domain: isDevelopment ? "http://localhost:8080" : "https://grithofmann.netlify.app",
   environment: process.env.NODE_ENV || "development", // Detects production or development
 };
